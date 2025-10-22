46f4d3c5a278017b8a2405c4e9837ff71ffffaef
Mario Gemoll <mariogemoll@mailbox.org>
2025-10-22 11:58:30 +0200

    Add functions to create standard DOM elements

--- src/dom.ts
+++ src/dom.ts
@@ -1,3 +1,10 @@
+type HTMLElementGenerator<T> = (
+  attrs: Record<string, string>, style?: Partial<CSSStyleDeclaration>
+) => T;
+type HTMLElementAdder<T> = (
+  parent: HTMLElement, attrs: Record<string, string>, style?: Partial<CSSStyleDeclaration>
+) => T;
+
 export function el(parent: Document | Element, query: string): Element {
   const element = parent.querySelector(query);
   if (!element) {
@@ -5,6 +12,48 @@ export function el(parent: Document | Element, query: string): Element {
   }
   return element;
 }
+
+export function makeEl(
+  tagName: string, attrs: Record<string, string>, style: Partial<CSSStyleDeclaration> = {}
+): HTMLElement {
+  const el = document.createElement(tagName);
+  for (const [key, value] of Object.entries(attrs)) {
+    el.setAttribute(key, value);
+  }
+  Object.assign(el.style, style);
+  return el;
+}
+
+export function addEl(
+  parent: HTMLElement,
+  tagName: string,
+  attrs: Record<string, string>,
+  style: Partial<CSSStyleDeclaration> = {}
+): HTMLElement {
+  const el = makeEl(tagName, attrs, style);
+  parent.appendChild(el);
+  return el;
+}
+
+export const addDiv: HTMLElementAdder<HTMLDivElement> = (parent, attrs, style = {}) => (
+  addEl(parent, 'div', attrs, style) as HTMLDivElement
+);
+
+export const addSpan: HTMLElementAdder<HTMLSpanElement> = (parent, attrs, style = {}) => (
+  addEl(parent, 'span', attrs, style) as HTMLSpanElement
+);
+
+export const makeCanvas: HTMLElementGenerator<HTMLCanvasElement> = (attrs, style = {}) => (
+  makeEl('canvas', attrs, style) as HTMLCanvasElement
+);
+
+export const addCanvas: HTMLElementAdder<HTMLCanvasElement> = (parent, attrs, style = {}) => (
+  addEl(parent, 'canvas', attrs, style) as HTMLCanvasElement
+);
+
+export const addTd: HTMLElementAdder<HTMLTableCellElement> = (parent, attrs, style = {}) => (
+  addEl(parent, 'td', attrs, style) as HTMLTableCellElement
+);
 
 export function removePlaceholder(container: HTMLElement): void {
   const placeholder = container.querySelector('.placeholder');
